d3e5e66c214daa0bcf60979109463c3a72c2f4e0
boer <brener.oliveira@gft.com>
2023-02-20 17:00:15 -0300

    Listeners ajustados

--- str-consumer/src/main/java/com/brener/strconsumer/listener/StrConsumerListener.java
+++ str-consumer/src/main/java/com/brener/strconsumer/listener/StrConsumerListener.java
@@ -9,8 +9,18 @@ import org.springframework.stereotype.Component;
 public class StrConsumerListener {
 
     // Informa que a mensagem foi lida no topic
+    @KafkaListener(groupId = "group-0", topics = "str-topic", containerFactory = "strContainerFactory")
+    public void create(String message) {
+        log.info("CREATE ::: Receive message {}", message);
+    }
+
     @KafkaListener(groupId = "group-1", topics = "str-topic", containerFactory = "strContainerFactory")
-    public void listener(String message) {
-        log.info("Receive message {}", message);
+    public void log(String message) {
+        log.info("LOG ::: Receive message {}", message);
+    }
+
+    @KafkaListener(groupId = "group-2", topics = "str-topic", containerFactory = "strContainerFactory")
+    public void history(String message) {
+        log.info("HISTORY ::: Receive message {}", message);
     }
 }
